9bd63994c18fdbc823c524f3663952080c8ca3ae
Fadzilah Rafiq <fadzilahrafiq@gmail.com>
2024-05-20 16:12:09 +0800

    Second commit - random updates, mainly on home page

--- src/pages/Home/Home.style.tsx
+++ src/pages/Home/Home.style.tsx
@@ -4,12 +4,99 @@ export default StyleSheet.create({
 
     container:{
         flex:1,
-        backgroundColor:'#AAC8A7'
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor:'#AAC8A7',
+        alignContent: 'flex-start',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 50
     
     },
     text:{
         color:'white',
         fontSize:24
     
+    },
+    header: {
+        width: '100%',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerLogo: {
+        width: 100,
+        height: 100,
+        marginBottom: 10,
+    },
+    headerText: {
+        width: '100%',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+    },
+    content: {
+        width: '100%',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        alignContent: 'flex-start',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        marginTop: 40
+    },
+    contentBox: {
+        width: '100%',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,.1)'
+    },
+    contentLogoContainer: {
+        width: '30%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    contentTextContainer: {
+        width: '70%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: 'black'
+    },
+    contentPlaceholderLogo: {
+        width: 70,
+        height: 70,
+        borderRadius: 50
+    },
+    contentTextCommon: {
+        width: '100%'
+    },
+    contentTextTitle: {
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+    contentTextSub: {
+        fontSize: 18
+    },
+    contentTextLink: {
+        fontSize: 12,
+        marginTop: 20,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'right',
     }
 })
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
-import {Image, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Image, View, Text} from 'react-native';
 import styles from './Home.style';
 
 const Home: React.FC = () => {
+  const [data, setData] = useState([]);
+  const [expiredData, setExpired] = useState([]);
+  const [nearingData, setNearing] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = await fetch('../../constants/sample-data.json');
+        // const jsonData = await response.json();
+        // setData(jsonData.itemList);
+        const response = require('../../constants/sample-data.json');
+        setData(response.itemList);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      
+      {/* HEADER CONTAINER */}
+      <View style={styles.header}>
+        <Image source={require('../../assets/images/default.png')} style={ styles.headerLogo } />
+        <Text style={styles.headerText}>YouGrocer</Text>
+      </View>
+      {/* CONTENT CONTAINER */}
+      <View style={styles.content}>
+        {/* CONTENT NEARING EXPIRY */}
+        <View style={styles.contentBox}>
+          <View style={styles.contentLogoContainer}>
+            <Image source={require('../../assets/images/default.png')} style={ styles.contentPlaceholderLogo } />
+          </View>
+          <View style={styles.contentTextContainer}>
+            <Text style={[styles.contentTextCommon, styles.contentTextTitle]}>Nearing Expiry</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextSub]}>n items are nearing expiry</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextLink]}>Click to view</Text>
+          </View>
+        </View>
+
+        {/* CONTENT EXPIRED */}
+        <View style={styles.contentBox}>
+          <View style={styles.contentLogoContainer}>
+            <Image source={require('../../assets/images/default.png')} style={ styles.contentPlaceholderLogo } />
+          </View>
+          <View style={styles.contentTextContainer}>
+            <Text style={[styles.contentTextCommon, styles.contentTextTitle]}>Expired Items</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextSub]}>n items have expired</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextLink]}>Click to review</Text>
+          </View>
+        </View>
+
+        {/* CONTENT ADD NEW ITEMS */}
+        <View style={styles.contentBox}>
+          <View style={styles.contentLogoContainer}>
+            <Image source={require('../../assets/images/default.png')} style={ styles.contentPlaceholderLogo } />
+          </View>
+          <View style={styles.contentTextContainer}>
+            <Text style={[styles.contentTextCommon, styles.contentTextTitle]}>Add Items</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextSub]}>Total { data.length } items are listed</Text>
+            <Text style={[styles.contentTextCommon, styles.contentTextLink]}>Click to add new items</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
